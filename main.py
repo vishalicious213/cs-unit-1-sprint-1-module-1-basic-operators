19bc42557cef5fd13e5dfd9b00a11761cffe7d08
Vish Singh <vishalicious213@gmail.com>
2020-10-05 21:22:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+1. Assign two different types to the variables "a" and "b".
+
+2. Use basic operators to create a list that contains three instances of "a" and three instances of "b".
+"""
+# Modify the code below to meet the requirements outlined above
+a = "string"
+b = 213
+
+my_list = [a, b] * 3
+print(my_list)
+print((my_list[0] *3), (my_list[1] *3))
